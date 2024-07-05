830024d9bb7f150a1340a43385e3908d8c86564f
Kanhaiya lal joshi <kanhaiya.joshi@vlinkinfo.com>
2024-07-05 12:06:21 +0530

    remove unused code

--- SalesAnalytics/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SalesAnalytics.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
